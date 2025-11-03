a4ef2f440190ba3582bf905e4226c95a25457b76
momo-694 <romelestoque3@gmail.com>
2025-11-03 13:22:31 +0900

    Declaring data s and components

--- MineSweeper/Minesweeper.java
+++ MineSweeper/Minesweeper.java
@@ -5,11 +5,28 @@ import java.util.Random;
 import java.util.ArrayList;
 
 public class Minesweeper extends JFrame {
+	class MineTile extends JButton {
+		int r, c;
+		
+		public MineTile(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+		
+	}
+	
+	//data
 	private final int tSize = 40;
 	private final int tRow = 8;
 	private final int tCol = 8;
-	private final int wHeight = tSize * tRow;
-	private final int wWidth = tSize * tCol;
+	private final int wHeight = tSize*tRow;
+	private final int wWidth = tSize*tCol;
+	private MineTile[][] = new MineTile[tRow][tCol];
+	private ArrayList<MineTile> mTiles;
+	
+	//components
+	private JLabel lblText;
+	private JPanel pnlText, pnlTiles;
 	
 	public Minesweeper() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
