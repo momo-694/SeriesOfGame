d7b5bf6ff46dc20779f0e37d1647be9371d740b3
momo-694 <romelestoque3@gmail.com>
2025-11-03 21:12:16 +0900

    Added mine tiles

--- MineSweeper/Minesweeper.java
+++ MineSweeper/Minesweeper.java
@@ -19,26 +19,49 @@ public class Minesweeper extends JFrame {
 	private final int tSize = 40;
 	private final int tRow = 8;
 	private final int tCol = 8;
-	private final int wHeight = tSize*tRow;
-	private final int wWidth = tSize*tCol;
+	private final int boardHeight = tSize*tRow;
+	private final int boardWidth = tSize*tCol;
 	private final int mineCount = 10;
-	private MineTile[][] mTiles = new MineTile[tRow][tCol];
+	private MineTile[][] board = new MineTile[tRow][tCol];
 	private ArrayList<MineTile> mineList;
 	
 	//components
 	private JLabel lblText;
-	private JPanel pnlText, pnlTiles;
+	private JPanel pnlText, pnlBoard;
 	
 	public Minesweeper() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Minesweeper");
 		setLocationRelativeTo(null);
-		setSize(wHeight, wWidth);
+		setSize(boardHeight, boardWidth);
 		setLayout(new BorderLayout());
 		
+		pnl = new JPanel(new BorderLayout());
 		lblText = new JLabel("Minesweeper" + mineCount);
+		pnlText.add(lblText);
+		
+		pnlBoard = new JPanel(new GridLayout(tRow, tCol));
+		setBoardTiles();
 		//TODO
 		
+		add(pnlText, BorderLayout.NORTH);
 		setVisible(true);
+	}
+	
+	public void setBoardTiles() {
+		for(int r = 0; r < tRow; r++) {
+			for(int c = 0; c < tCol; c++) {
+				MineTile tile = new MineTile(r, c);
+				tile.setFocusable(false);
+				tile.setFont(new Font("Arial Unicode MS", Font.PLAIN, 45));
+				tile.addMouseListener(new Mouse adapter() {
+					@Override
+					public void mousePressed(Mouse event e) {
+						//TODO
+					}
+				});
+			}
+			pnlBoard.add(tile);
+		}
 	}
 }
