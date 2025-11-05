f583e817e55abca5ef485fa1f00980634caa0f6b
momo-694 <romelestoque3@gmail.com>
2025-11-05 18:45:58 +0800

    Added difficulty and fixed frame

--- MineSweeper/Minesweeper.java
+++ MineSweeper/Minesweeper.java
@@ -16,14 +16,14 @@ public class Minesweeper extends JFrame {
 	}
 	
 	//data
-	private final int tSize = 40;
-	private final int tRow = 8;
-	private final int tCol = 8;
-	private final int boardHeight = tSize*tRow;
-	private final int boardWidth = tSize*tCol;
-	private final int mineCount = 10;
-	private int flagRemaining = mineCount;
-	private MineTile[][] board = new MineTile[tRow][tCol];
+	private final int tSize = 36;
+	private int tRow;
+	private int tCol;
+	private int boardHeight;
+	private int boardWidth;
+	private int mineCount;
+	private int flagRemaining;
+	private MineTile[][] board;
 	private ArrayList<MineTile> mineList;
 	private int tileClicked = 0;
 	private boolean isGameOver = false;
@@ -31,19 +31,85 @@ public class Minesweeper extends JFrame {
 	private String time = "00:00:00";
 
 	//components
-	private JLabel lblTime, lblFlag;
-	private JPanel pnlText, pnlBoard, pnlGameOver, pnlRetryButton;
+	private JLabel lblTime, lblFlag, lblDifficulty;
+	private JPanel pnlText, pnlBoard, pnlGameOver, pnlRetryButton, pnlDifficulty;
 	private Timer timer;
-	private JFrame tryAgainFrame;
+	private JFrame tryAgainFrame, difficultyFrame;
 	
 	public Minesweeper() {
+		showDifficulties();
+	}
+
+	public void showDifficulties() {
+		difficultyFrame = new JFrame("Mine");
+
+		difficultyFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		difficultyFrame.setSize(250, 300);
+		difficultyFrame.setLocationRelativeTo(null);
+		difficultyFrame.setLayout(new FlowLayout());
+		difficultyFrame.getContentPane().setBackground(Color.GRAY);
+
+		lblDifficulty = new JLabel("SELECT DIFFICULTY");
+		lblDifficulty.setFont(new Font("Times New Roman", Font.BOLD, 20));
+
+		pnlDifficulty = new JPanel(new GridLayout(4, 1, 0, 6));
+		pnlDifficulty.setBackground(Color.GRAY);
+		pnlDifficulty.add(new JButton("EASY"));
+		pnlDifficulty.add(new JButton("MEDUIM"));
+		pnlDifficulty.add(new JButton("HARD"));
+		pnlDifficulty.add(new JButton("EXTREME"));
+
+		for(Component com : pnlDifficulty.getComponents()) {
+			((JButton) com).setBackground(Color.LIGHT_GRAY);
+			((JButton) com).setFocusable(false);
+			((JButton) com).setPreferredSize(new Dimension(170, 50));
+			((JButton) com).setFont(new Font("Times New Roman", Font.PLAIN, 16));
+			((JButton) com).addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					switch(e.getActionCommand().toLowerCase()) {
+						case "easy":
+							tRow = 8;
+							mineCount = 10;
+							break;
+						case "meduim":
+							tRow = 10;
+							mineCount = 15;
+							break;
+						case "hard":
+							tRow = 12;
+							mineCount = 20;
+							break;
+						case "extreme":
+							tRow = 18;
+							mineCount = 50;
+							break;
+					}
+					tCol = tRow;
+					flagRemaining = mineCount;
+					boardHeight = tRow*tSize;
+					boardWidth = tCol*tSize;
+					difficultyFrame.dispose();
+					initGame();
+				}
+			});
+		}
+
+		difficultyFrame.add(lblDifficulty);
+		difficultyFrame.add(pnlDifficulty);
+		difficultyFrame.setVisible(true);
+	}
+
+	public void initGame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Minesweeper");
 		setSize(boardHeight, boardWidth);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setLayout(new BorderLayout());
-		
+
+		board = new MineTile[tRow][tCol];
+					
 		pnlText = new JPanel(new GridLayout(1, 2));
 		pnlText.setBackground(Color.GRAY);
 		lblTime = new JLabel("TIME: " + time);
@@ -94,12 +160,14 @@ public class Minesweeper extends JFrame {
 								Icon flag = (Icon) new ImageIcon("flag.png");
 
 								if(flagRemaining >= 0) {
-									if(tile.getIcon() == null && tile.isEnabled()) {
-										tile.setIcon(flag);
-										--flagRemaining;
-									}else if(tile.getIcon().toString().equals(flag.toString())) {
-										tile.setIcon(null);
-										++flagRemaining;
+									if(tile.isEnabled()) {
+										if(tile.getIcon() == null) {
+											tile.setIcon(flag);
+											--flagRemaining;
+										}else if(tile.getIcon().toString().equals(flag.toString())) {
+											tile.setIcon(null);
+											++flagRemaining;
+										}
 									}
 									lblFlag.setText("FLAG LEFT: " + flagRemaining);
 								}
@@ -142,7 +210,7 @@ public class Minesweeper extends JFrame {
 
 		MineTile tile = board[r][c];
 
-		if(!tile.isEnabled()) {
+		if(!tile.isEnabled() || tile.getIcon()!=null) {
 			return;
 		}
 
